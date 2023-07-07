9138e9cada5c1e60e72f04993cc9248e8638fe2c
Younes Nourzehi <unesnz@gmail.com>
2023-07-07 09:10:27 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const debug = require("debug")("app:server");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Initialize DB
+require("./db")();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  debug(`Server started on port ${PORT}`);
+});
---
+++ db.js
@@ -0,0 +1,22 @@
+const debug = require("debug")("app:db");
+const mongoose = require("mongoose");
+
+module.exports = () => {
+  mongoose
+    .connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => debug("MongoDB Connected..."))
+    .catch((err) => debug(`ERROR connecting to MongoDB: ${err.message}`));
+
+  mongoose.connection.on("connected", () => debug("Mongoose connected to db"));
+
+  mongoose.connection.on("error", (err) =>
+    debug(`ERROR connecting Mongoose: ${err.message}`)
+  );
+
+  mongoose.connection.on("disconnected", () =>
+    debug("Mongoose is disconnected...")
+  );
+};
