cd699c47d7d459b7ec2385a54867b5e2972e0083
Younes Nourzehi <unesnz@gmail.com>
2023-07-07 11:00:25 +0300

    define user route

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.use(express.urlencoded({ extended: true }));
 //Initialize DB
 require("./db")();
 
+const userRoute = require("./Routes/User.route");
+app.use("/users/", userRoute);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   debug(`Server started on port ${PORT}`);
