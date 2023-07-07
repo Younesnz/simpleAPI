2709c00f3d56a9bd45f08eb0d65e0b764e438a0b
Younes Nourzehi <unesnz@gmail.com>
2023-07-07 09:35:33 +0300

    build user model

--- Models/User.model.js
+++ Models/User.model.js
@@ -1,2 +1,42 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Joi = require("joi");
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    min: 0,
+    max: 200,
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+const validateUser = () => {
+  const joiSchema = {
+    username: Joi.string().min(5).max(50).required(),
+    password: Joi.string().required(),
+    age: Joi.number().min(0).max(200),
+    isAdmin: Joi.boolean().required(),
+  };
+};
+
+exports.User = User;
+exports.validate = validateUser;
